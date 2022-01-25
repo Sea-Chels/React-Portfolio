edf4ac03bd17cc3cba20d87d92ff03e9da8e9f2e
MissingNewt <chelby.sallady@gmail.com>
2022-01-24 22:59:48 -0800

    adding react files and attaching photos

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Main_Page from './Components/Main-Page'
+const App = ()=>{
+  return (
+    <div className="App">
+        <Main_Page/>
+    </div>
+  );
+}
+
+export default App;
